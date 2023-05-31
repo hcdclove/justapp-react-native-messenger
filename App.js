d57effaff3f3a8be8655113518926d28b8e9bdb8
hcdclove <131356756+hcdclove@users.noreply.github.com>
2023-05-31 15:32:59 -0400

    JustApp Version 1.0

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        console.log('Before');
+        await Font.loadAsync({
+          black: require('./assets/fonts/Roboto-Black.ttf'),
+          regular: require('./assets/fonts/Roboto-Regular.ttf'),
+          medium: require('./assets/fonts/Roboto-Medium.ttf'),
+          bold: require('./assets/fonts/Roboto-Bold.ttf'),
+          light: require('./assets/fonts/Roboto-Light.ttf'),
+          thin: require('./assets/fonts/Roboto-Thin.ttf'),
+          lightItalic: require('./assets/fonts/Roboto-LightItalic.ttf'),
+          mediumItalic: require('./assets/fonts/Roboto-MediumItalic.ttf'),
+          boldItalic: require('./assets/fonts/Roboto-BoldItalic.ttf'),
+          thinItalic: require('./assets/fonts/Roboto-ThinItalic.ttf'),
+          blackItalic: require('./assets/fonts/Roboto-BlackItalic.ttf')
+        });
+      } catch (error) {
+        console.log.error(error);
+      } finally {
+        console.log('Fonts loaded successfully');
+        setAppIsLoaded(true);
+      }
+    };
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      console.log('App is loaded');
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    console.log('App is  not loaded');
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
+      <SafeAreaView>
+        <Text style={styles.label}>Hi everyone!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +61,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  label: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: 'regular'
+  }
 });
